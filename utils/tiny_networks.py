55121dabd87ea79443db57bf5fbc3fb270de5c6c
Marten Nilsson (mnin) <marten.nilsson@tobii.com>
2018-03-01 14:08:57 +0100

    Tiny networks debugged, will be extended

--- utils/tiny_networks.py
+++ utils/tiny_networks.py
@@ -3,31 +3,31 @@ import torch.nn.functional as F
 import torch
 
 def refpad(x, pad):
-    return F.pad(x, pad, mode="reflect")
+    return F.pad(x, [0, 0, pad, pad], mode="reflect")
 
 class DSConv(nn.Module):
     def __init__(self, in_channels, out_channels, filter_size, padding=0, stride=1):
         super().__init__()
-        self.pad = padding
+        self.pad = nn.ReflectionPad2d(padding)
 
         self.depthwise = nn.Conv2d(in_channels, in_channels, filter_size,
                 stride=stride, groups=in_channels)
         self.widthwise_fc = nn.Conv2d(in_channels, out_channels, 1)
 
     def forward(self, batch):
-        return self.widthwise_fc(self.depthwise(refpad(batch, self.pad)))
+        return self.widthwise_fc(self.depthwise(self.pad(batch)))
 
 class DSConvTranspose(nn.Module):
     def __init__(self, in_channels, out_channels, filter_size, padding=0, stride=1):
         super().__init__()
-        self.pad = padding
+        self.pad = nn.ReflectionPad2d(padding)
 
         self.widthwise_fc = nn.Conv2d(in_channels, out_channels, 1)
         self.depthwise = nn.ConvTranspose2d(out_channels, out_channels, 
                 filter_size, stride=stride, groups=out_channels)
 
     def forward(self, batch):
-        return self.depthwise(refpad(self.widthwise_fc(batch), self.pad))
+        return self.depthwise(self.pad(self.widthwise_fc(batch)))
 
 
 # Encoders ---------------------------------------
@@ -35,9 +35,9 @@ class DSEncoder(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.down1 = DSConv(16, 32, 4, padding=2, stride=4)
-        self.down2 = DSConv(32, 64, 4, padding=2, stride=4)
-        self.down3 = DSConv(64, 128, 4, padding=2, stride=4)
+        self.down1 = DSConv(16, 32, 4, padding=0, stride=4)
+        self.down2 = DSConv(32, 64, 4, padding=0, stride=4)
+        self.down3 = DSConv(64, 128, 4, padding=0, stride=4)
 
         self.deflate = nn.Conv2d(128, 128, 4)
 
@@ -57,9 +57,9 @@ class DSDecoder(nn.Module):
 
         self.inflate = nn.ConvTranspose2d(128, 128, 4) # Latent -> 4x4
 
-        self.up1 = DSConvTranspose(128, 64, 4, padding=2, stride=4) #4x4->16x16
-        self.up2 = DSConvTranspose(64, 32, 4, padding=2, stride=4) #16x16->64x64
-        self.up3 = DSConvTranspose(32, 16, 4, padding=2, stride=4) #64x64->256x256
+        self.up1 = DSConvTranspose(128, 64, 4, padding=0, stride=4) #4x4->16x16
+        self.up2 = DSConvTranspose(64, 32, 4, padding=0, stride=4) #16x16->64x64
+        self.up3 = DSConvTranspose(32, 16, 4, padding=0, stride=4) #64x64->256x256
 
     def forward(self, latent):
         img = F.leaky_relu(F.normalize(self.inflate(latent)), 0.2)
@@ -74,13 +74,13 @@ class DSDiscriminator(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.down1 = DSConv(16, 32, 4, padding=2, stride=4)
-        self.down2 = DSConv(32, 64, 4, padding=2, stride=4)
-        self.down3 = DSConv(64, 128, 4, padding=2, stride=4)
+        self.down1 = DSConv(16, 32, 4, padding=0, stride=4)
+        self.down2 = DSConv(32, 64, 4, padding=0, stride=4)
+        self.down3 = DSConv(64, 128, 4, padding=0, stride=4)
 
         self.deflate = nn.Conv2d(128, 128, 4)
 
-        self.to_pred = nn.Linear(128, 128)
+        self.to_pred = nn.Linear(128, 1)
 
     def forward(self, img):
         img = F.leaky_relu(self.down1(img), 0.2)
