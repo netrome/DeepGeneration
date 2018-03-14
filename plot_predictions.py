423bdadb368f6fb61f14c2b4342b501c70107f9d
Marten Nilsson (mnin) <marten.nilsson@tobii.com>
2018-03-14 14:39:16 +0100

    Small plot change

--- plot_predictions.py
+++ plot_predictions.py
@@ -8,7 +8,7 @@ state = json.load(open(file, "r"))
 
 plt.plot(state["history_real"], label="Real predictions")
 plt.plot(state["history_fake"], label="Fake predictions")
-plt.axis([100, 200, -10, 100])
+plt.axis([100, 200, -10, 75])
 plt.legend()
 plt.xlabel("Iteration")
 plt.ylabel("Discriminator output")
