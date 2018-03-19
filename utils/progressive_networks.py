62d06e6648d4b4a1937c8a78197679a2154111f5
Marten Nilsson (mnin) <marten.nilsson@tobii.com>
2018-03-19 10:13:43 +0100

    Changed order of normalization in trivial networks

--- utils/progressive_networks.py
+++ utils/progressive_networks.py
@@ -4,14 +4,17 @@ import torch.nn.functional as F
 
 
 class TrivialUpBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, activation):
+    def __init__(self, in_channels, out_channels, activation, normalize=False):
         super().__init__()
+        self.normalize = normalize
         self.activation = activation
         self.upconv = nn.ConvTranspose2d(in_channels, in_channels, 2, stride=2)
         self.thinner = nn.Conv2d(in_channels, out_channels, 3, padding=1)
 
     def forward(self, feature_map):
-        return self.thinner(F.normalize(self.activation(self.upconv(feature_map))))
+        if self.normalize:
+            return self.thinner(self.activation(F.normalize(self.upconv(feature_map))))
+        return self.thinner(self.activation(self.upconv(feature_map)))
 
 
 class UpsamplingUpBlock(nn.Module):
@@ -28,13 +31,16 @@ class UpsamplingUpBlock(nn.Module):
 
 
 class TrivialDownBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, activation):
+    def __init__(self, in_channels, out_channels, activation, normalize=False):
         super().__init__()
+        self.normalize = normalize
         self.activation = activation
         self.fattener = nn.Conv2d(in_channels, out_channels, 3, padding=1)
         self.conv = nn.Conv2d(out_channels, out_channels, 2, stride=2)
 
     def forward(self, feature_map):
+        if self.normalize:
+            return self.conv(self.activation(F.normalize(self.fattener(feature_map))))
         return self.conv(self.activation(self.fattener(feature_map)))
 
 
@@ -147,21 +153,21 @@ class TrivialGeneratorLight(nn.Module):
         self.inflate = nn.ConvTranspose2d(128, 128, 4)  # Latent -> 4x4
         self.low_conv = nn.Conv2d(128, 128, 3, padding=1)  # Preserve shape
 
-        self.up1 = TrivialUpBlock(128, 112, self.activation)  # 4x4 -> 8x8
-        self.up2 = TrivialUpBlock(112, 96, self.activation)  # 8x8 -> 16x16
-        self.up3 = TrivialUpBlock(96, 80, self.activation)  # 16x16 -> 32x32
-        self.up4 = TrivialUpBlock(80, 64, self.activation)  # 32x32 -> 64x64
-        self.up5 = TrivialUpBlock(64, 32, self.activation)  # 64x64 -> 128x128
+        self.up1 = TrivialUpBlock(128, 112, self.activation, True)  # 4x4 -> 8x8
+        self.up2 = TrivialUpBlock(112, 96, self.activation, True)  # 8x8 -> 16x16
+        self.up3 = TrivialUpBlock(96, 80, self.activation, True)  # 16x16 -> 32x32
+        self.up4 = TrivialUpBlock(80, 64, self.activation, True)  # 32x32 -> 64x64
+        self.up5 = TrivialUpBlock(64, 32, self.activation, True)  # 64x64 -> 128x128
         self.up6 = TrivialUpBlock(32, 16, self.activation)  # 128x128 -> 256x256
 
         self.blocks = [self.up1, self.up2, self.up3, self.up4, self.up5, self.up6]
 
     def forward(self, z, levels=6):
-        img = F.normalize(self.activation(self.inflate(z)))
-        img = F.normalize(self.activation(self.low_conv(img)))
+        img = self.activation(self.inflate(z)))
+        img = self.activation(F.normalize(self.low_conv(img)))
 
         for i in range(levels):
-            img = F.normalize(self.activation(self.blocks[i](img)))
+            img = self.activation(self.blocks[i](img))
         return img
 
     def fade_in(self, z, levels=6):
@@ -202,11 +208,11 @@ class TrivialDiscriminatorLight(nn.Module):
         self.fc = nn.Linear(128, 1)
 
         self.down1 = TrivialDownBlock(16, 32, self.activation)  # 256x256 -> 128x128
-        self.down2 = TrivialDownBlock(32, 64, self.activation)  # 128x128 -> 64x64
-        self.down3 = TrivialDownBlock(64, 80, self.activation)  # 64x64 -> 32x32
-        self.down4 = TrivialDownBlock(80, 96, self.activation)  # 32x32 -> 16x16
-        self.down5 = TrivialDownBlock(96, 112, self.activation)  # 16x16 -> 8x8
-        self.down6 = TrivialDownBlock(112, 127, self.activation)  # 8x8 -> 4x4
+        self.down2 = TrivialDownBlock(32, 64, self.activation, True)  # 128x128 -> 64x64
+        self.down3 = TrivialDownBlock(64, 80, self.activation, True)  # 64x64 -> 32x32
+        self.down4 = TrivialDownBlock(80, 96, self.activation, True)  # 32x32 -> 16x16
+        self.down5 = TrivialDownBlock(96, 112, self.activation, True)  # 16x16 -> 8x8
+        self.down6 = TrivialDownBlock(112, 127, self.activation, True)  # 8x8 -> 4x4
 
         self.blocks = [self.down1, self.down2, self.down3, self.down4, self.down5, self.down6]
 
