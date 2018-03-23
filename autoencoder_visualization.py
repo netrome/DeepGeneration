c3abebd23990d4c46d80b7fc2bf6d42a11cd187a
Marten Nilsson (mnin) <marten.nilsson@tobii.com>
2018-03-23 14:34:39 +0100

    Autoencoder visualization finished

--- autoencoder_visualization.py
+++ autoencoder_visualization.py
@@ -8,6 +8,7 @@ import utils.utils as u
 import utils.datasets as datasets
 
 import settings
+import os
 
 E = u.create_encoder()
 G = u.create_generator()
@@ -17,11 +18,14 @@ fromRGB = nn.Conv2d(2, 16, 1)
 
 dataset = u.get_data_set()
 torch.random.manual_seed(1337)
-data_loader = torch.utils.data.dataloader.DataLoader(dataset, batch_size=4, shuffle=True)
+data_loader = torch.utils.data.dataloader.DataLoader(dataset, batch_size=8, shuffle=True)
+
+G.load_state_dict(torch.load(os.path.join(settings.MODEL_PATH, "G.params")))
+E.load_state_dict(torch.load(os.path.join(settings.MODEL_PATH, "E.params")))
+toRGB.load_state_dict(torch.load(os.path.join(settings.MODEL_PATH, "toRGB6.params")))
+fromRGB.load_state_dict(torch.load(os.path.join(settings.MODEL_PATH, "fromRGB6.params")))
 
 if settings.CUDA:
-    E.cuda()
-    G.cuda()
     toRGB.cuda()
     fromRGB.cuda()
 
@@ -33,7 +37,9 @@ for batch in data_loader:
     encoded = E(fromRGB(batch))[0]
     decoded = toRGB(G(encoded.view(-1, 128, 1, 1)))
 
-    single = make_grid(decoded[:, 0].data.cpu().contiguous().view(4, 1, 256, 256))
+    original = make_grid(batch[:, 0].data.cpu().contiguous().view(8, 1, 256, 256))
+    single = make_grid(decoded[:, 0].data.cpu().contiguous().view(8, 1, 256, 256))
+    save_image(original, "/tmp/original.png")
     save_image(single, "/tmp/decoded.png")
     break
 
