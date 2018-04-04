a114cf107220b8ac287c46d29827e35fb47de0d4
Marten Nilsson (mnin) <marten.nilsson@tobii.com>
2018-04-04 09:25:07 +0200

    Small fixes

--- toy_experiments/test_model.py
+++ toy_experiments/test_model.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
@@ -5,9 +6,9 @@ import matplotlib.pyplot as plt
 import networks
 import iris
 
-encoder = networks.encoder()
-decoder = networks.decoder()
-latent = torch.FloatTensor(200, 1).random_()  # Latent codes to generate new data
+encoder = networks.encoder
+decoder = networks.decoder
+latent = Variable(torch.FloatTensor(200, 1).random_(), volatile=True)  # Latent codes to generate new data
 
 # Load networks
 encoder.load_state_dict(torch.load("saved_nets/vae_encoder.params"))
@@ -16,10 +17,10 @@ decoder.load_state_dict(torch.load("saved_nets/vae_decoder.params"))
 # Get original data
 X, Y = iris.get_train_data()
 data = np.concatenate([X, Y.reshape([75, 1])], axis=1)
-complete_batch = Variable(torch.from_numpy(data).float())
+complete_batch = Variable(torch.from_numpy(data).float(), volatile=True)
 
 # Encode-decode data
-encoded = encoder(Variable(complete_batch, volatile=True))[:, 0]
+encoded = encoder(complete_batch)[:, 0].contiguous().view(75, 1)
 decoded = decoder(encoded)
 
 # Generate new data
@@ -28,7 +29,7 @@ generated = decoder(latent)
 
 # Convert generated data to numpy
 decoded = decoded.data.numpy()
-generated = generated.dara.numpy()
+generated = generated.data.numpy()
 
 plt.scatter(X[:, 0], X[:, 1], c=Y)
 plt.figure()
