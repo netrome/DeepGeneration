742b7f78c690437fab14108e20e3ec2adddd4ea4
Marten Nilsson (mnin) <marten.nilsson@tobii.com>
2018-04-12 14:35:40 +0200

    Adapted quantitative tests for classification in iris

--- toy_experiments/iris/quantitative_test.py
+++ toy_experiments/iris/quantitative_test.py
@@ -18,8 +18,8 @@ decoder.load_state_dict(torch.load("saved_nets/{}_decoder.params".format(sys.arg
 # Get original data
 X, Y = iris.get_train_data()
 X_tst, Y_tst = iris.get_test_data()
-data = np.concatenate([X, Y.reshape([75, 1])], axis=1)
-data_tst = np.concatenate([X_tst, Y_tst.reshape([75, 1])], axis=1)
+data = np.concatenate([X, Y.reshape([75, 3])], axis=1)
+data_tst = np.concatenate([X_tst, Y_tst.reshape([75, 3])], axis=1)
 complete_batch = Variable(torch.from_numpy(data).float())
 test_batch = Variable(torch.from_numpy(data_tst).float())
 
@@ -29,23 +29,24 @@ generated = decoder(latent)
 # create train set
 if "augment" in sys.argv:
     patterns = torch.cat((generated[:, :2], complete_batch[:, :2]), 0)
-    targets = torch.cat((generated[:, 2], complete_batch[:, 2]), 0).view(275, 1)
+    targets = torch.cat((generated[:, 2:], complete_batch[:, 2:]), 0).view(275, 3)
 elif "original" in sys.argv:
     patterns = complete_batch[:, :2]
-    targets = complete_batch[:, 2].contiguous().view(75, 1)
+    targets = complete_batch[:, 2:].contiguous().view(75, 3)
 elif "gen" in sys.argv:
     patterns = generated[:, :2]
-    targets = generated[:, 2].contiguous().view(200, 1)
+    targets = generated[:, 2:].contiguous().view(200, 3)
 
 patterns = patterns.detach()
-targets = targets.detach()
+targets = targets.detach().max(1)[1]  # Convert from ohe to indices again
 
 opt = torch.optim.Adam(classifier.parameters())
 
 iters = 10000
+criterion = torch.nn.CrossEntropyLoss()
 for i in range(iters):
     pred = classifier(patterns)
-    loss = torch.mean((pred - targets)**2)
+    loss = criterion(pred, targets)
 
     opt.zero_grad()
     loss.backward()
@@ -57,6 +58,8 @@ print()
 print("------")
 # Evaluate accuracy
 test_pred = classifier(test_batch[:, :2])
-mse = torch.mean((test_pred - test_batch[:, 2].contiguous().view(75, 1)) ** 2)
-print("Mse: {}".format(float(mse)))
+pred_idices = test_pred.max(1)[1]
+test_targets = test_batch[:, 2:].max(1)[1]
+corrects = int((pred_idices == test_targets).sum())
+print("Corrects: {}/{}".format(corrects, len(test_targets)))
 
